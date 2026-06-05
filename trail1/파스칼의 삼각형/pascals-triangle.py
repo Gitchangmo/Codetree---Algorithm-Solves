6116a5e17ba4c6b29118fd867ce5152acab4fc87
김창모 KIM CHANGMO <333rlackdah@gmail.com>
2026-06-05 16:48:10 +0900

    [Passed] 파스칼의 삼각형 (48ms, 16MB)

--- trail1/파스칼의 삼각형/pascals-triangle.py
+++ trail1/파스칼의 삼각형/pascals-triangle.py
@@ -8,7 +8,10 @@ for i in range(N):
 
 for i in range(1, N):
     for j in range(1, i+1):
-        arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+        if j == 0:
+            arr[i][j] = 1
+        else:
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
 for i in range(N):
     for j in range(i+1):
