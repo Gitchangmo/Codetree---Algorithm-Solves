cd52e2283cef922ce2db8bddc9fc9e8bb2b83219
김창모 KIM CHANGMO <333rlackdah@gmail.com>
2026-06-11 23:40:36 +0900

    [Passed] 최소공배수 구하기 (49ms, 16MB)

--- trail2/최소공배수 구하기/find-the-least-common-multiple.py
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.py
@@ -3,11 +3,11 @@ n, m = map(int, input().split())
 # Please write your code here.
 def LCM(n, m):
     gcd = 0
-    for i in range(max(n, m), 0, -1):
+    for i in range(max(n, m), 0, -1):   # GCD 구하는 부분
         if n % i == 0 and m % i == 0:
             gcd = i
             break
 
-    print(n * m // gcd)
+    print(n * m // gcd)                 # LCM 구하는 부분 
 
 LCM(n, m)
